cbec999cb93d412380a26ec13302036b4b635e61
kelicaleb <kelicaleb7@gmail.com>
2023-01-15 18:09:23 +0300

    Completed Assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -8,19 +8,14 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
-
+  const color = colors.map(color => {
+   return  <li style={{color:color}}>{color}</li>
+  });
   return (
-    <div>
-      <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
-    </div>
-  );
+    <><h3>Css style color</h3><ul>
+      {color}
+    </ul></>
+  )
 }
 
 export default ColorList;
